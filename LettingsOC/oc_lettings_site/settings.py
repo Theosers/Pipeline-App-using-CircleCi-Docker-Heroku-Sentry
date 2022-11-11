7e5ec0f68d48b0cc56a7e930dab9135e2fa85be5
theophile <theophile.sers@yahoo.com>
2022-11-11 18:05:49 +0100

    Dockerfile updated

--- LettingsOC/oc_lettings_site/settings.py
+++ LettingsOC/oc_lettings_site/settings.py
@@ -65,8 +65,8 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'theophile',
+        'NAME': 'lettings',
+        'USER': 'theo',
         'PASSWORD': '123456',
         'HOST': 'db',
         'PORT': '5432',
@@ -110,6 +110,7 @@ USE_I18N = True
 
 USE_L10N = True
 
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
